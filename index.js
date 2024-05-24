6470dafe9b0b3bc41d716d577959f62e5e5edddd
Amaayu <namdevaniket446@gmail.com>
2024-05-24 18:30:55 +0530

    deleted files

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
+const User = require("./model/userModel"); // Ensure this path is correct
+const usersRouter = require("./routes/userRouts"); // Adjust the path if necessary
 const app = express();
+const bodyParser = require("body-parser");
+
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  res.send("Hello Aashu ");
-});
+// midelwares
 
-app.listen(3000, () => {
-  console.log("server is started at port-> 3000");
+app.use("/users", usersRouter);
+
+mongoose
+  .connect(
+    `mongodb+srv://namdevaniket446:igF55jty52X8eiGz@hook-db.rbfzfd3.mongodb.net/?retryWrites=true&w=majority&appName=hook-db`
+  )
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
---
+++ routes/userRouts.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express.Router();
+const User = require("../model/userModel"); // Ensure this path is correct
+app.post("/", async (req, res) => {
+  try {
+    const newUser = new User({
+      username: req.body.username, // Make sure 'username' is provided
+      email: req.body.email,
+      password: req.body.password,
+    });
+    await newUser.save();
+    console.log("done bhai");
+    res.status(201).send("User created successfully");
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).send("Error creating user");
+  }
+});
+
+module.exports = app;
